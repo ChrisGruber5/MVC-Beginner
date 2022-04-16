54d637573020cfd9cd7bbed3dd9b5fba3ecdefe5
Chris Gruber <chris_gruber@ou.edu>
2022-04-15 19:16:18 -0500

    Add project files.

---
+++ MVC Beginner/Controllers/PokemonController.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Beginner.Controllers
+{
+    public class PokemonController : Controller
+    {
+        // GET: Pokemon
+        public ActionResult Index()
+        {
+            PokemonAPI allpokemon;
+            using (var client = new HttpClient())
+            {
+                var json = client.GetStringAsync("https://pokeapi.co/api/v2/pokemon").Result;
+                allpokemon = JsonConvert.DeserializeObject<PokemonAPI>(json);
+            }
+            return View(allpokemon.results);
+        }
+    }
+}
---
+++ MVC Beginner/Models/PokemonInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Beginner.Models
+{
+    internal class PokemonDetails
+    {
+
+        public string name { get; set; }
+        public int height { get; set; }
+        public double weight { get; set; }
+
+        public Sprite sprites { get; set; }
+
+
+    }
+
+    public class Sprite
+    {
+        public string back_default { get; set; }
+
+        public string front_default { get; set; }
+    }
+}
